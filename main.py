d5f9755f536cb5ff56a9d37d13c5aeeb01046f29
Deepanshu <luckydeepanshu5@gmail.com>
2025-08-27 11:35:21 +0530

    query parameters

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ def blog(id : int):
     return {'data': id}
 
 
+@app.get('/blog')
+def blog(limit, published : bool):
+    
+    if published:
+        return {'data': f'{limit} published fetched from the db'}
+    else:
+        return {'data': f'{limit} fetched from the db'}
+
+
 @app.get('/blog/{id}/comments')
 def comments(id):
     return {'data': {'1', '2'}}
